f4d9ad1cbee3070d6582fde9d3e59a95dc72998f
amosyang <amosyang@shunshunliuxue.com>
2018-08-30 18:36:15 +0800

    first commit

---
+++ snippets/serializers.py
@@ -0,0 +1,37 @@
+from rest_framework import serializers
+from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
+
+# class SnippetSerializer(serializers.Serializer): # serializers提供序列化或反序列化json等方法
+#     # 每一个表都可以建一个serializer，类似Django的Form  专门用于json
+#     id = serializers.IntegerField(read_only=True)
+#     title = serializers.CharField(required=False, allow_blank=True, max_length=100)
+#     code = serializers.CharField(style={'base_template': 'textarea.html'})
+#     linenos = serializers.BooleanField(required=False)
+#     language = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default='python')
+#     style = serializers.ChoiceField(choices=STYLE_CHOICES, default='friendly')
+
+#     def create(self, validated_data):
+#         """
+#          根据提供的验证过的数据创建并返回一个新的`Snippet`实例。
+#         """
+#         return Snippet.objects.create(**validated_data)
+
+#     def update(self, instance, validated_data):
+#         """
+#         根据提供的验证过的数据更新和返回一个已经存在的`Snippet`实例。
+#         """
+#         instance.title = validated_data.get('title', instance.title)
+#         instance.code = validated_data.get('code', instance.code)
+#         instance.linenos = validated_data.get('linenos', instance.linenos)
+#         instance.language = validated_data.get('language', instance.language)
+#         instance.style = validated_data.get('style', instance.style)
+#         instance.save()
+#         return instance
+
+# 使用 ModelSerializer 改下之上方法
+class SnippetSerializer(serializers.ModelSerializer):
+    # ModelSerializer和Django中ModelForm功能相似
+    # Serializer和Django中Form功能相似
+    class Meta:
+        model = Snippet
+        fields = ('id', 'title', 'code', 'linenos', 'language', 'style')
